0224b0fbd90d7076bc67ec4e6f6ec5d7ecc7bcf8
catdoglove <34932357+catdoglove@users.noreply.github.com>
2018-03-05 16:26:08 +0900

    로딩 배경음 FadeOut
    음악이 점점 작게

---
+++ _Script/SoundBGEvt.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundBGEvt : MonoBehaviour {
+	
+	public AudioSource loadingBG;
+	public int aaa = 10;
+	int ckBG = 0;
+
+	// Use this for initialization
+	void Start () {
+		loadingBG = this.gameObject.GetComponent<AudioSource> ();		
+	}
+
+	void FixedUpdate(){
+		if (ckBG == 99) {
+			if (loadingBG.volume >= 0) {
+				loadingBG.volume = loadingBG.volume - (Time.deltaTime / (aaa - 7));
+			} else {
+				Destroy (this);
+				ckBG = 0;
+			}
+		}
+	}
+
+
+	public void soundFadeOut(){
+		ckBG = 99;
+	}
+}
