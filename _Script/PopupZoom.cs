78ff0b9e972243b470506edbe0938ce031086a37
catdoglove <34932357+catdoglove@users.noreply.github.com>
2018-05-25 18:00:20 +0900

    창들 효과 지정
    줌인아웃 하는중

---
+++ _Script/PopupZoom.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopupZoom : MonoBehaviour
+{
+    public GameObject popupWindow;
+    public GameObject GM;
+    Vector3 scalee;
+    private Exception e;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    public void ZoomIn()
+    {
+        scalee = transform.localScale;
+        StartCoroutine("popupZoomIn");
+
+    }
+
+    public void ZoomOut()
+    {
+        StartCoroutine("popupZoomOut");
+    }
+    
+
+    IEnumerator popupZoomIn()
+    {
+        
+        scalee = transform.localScale;
+        scalee.x = 0.8f;
+        scalee.y = 0.8f;
+        transform.localScale = scalee;
+        while (scalee.x < 1f)
+        {
+            yield return new WaitForSeconds(0.01f);
+            scalee.x = scalee.x + 0.05f;
+            scalee.y = scalee.y + 0.05f;
+            transform.localScale = scalee;
+        }
+    }
+
+    IEnumerator popupZoomOut()
+    {
+        scalee = transform.localScale;
+        while (scalee.x >= 0.5f)
+        {
+            yield return new WaitForSeconds(0.0001f);
+            scalee.x = scalee.x - 0.08f;
+            scalee.y = scalee.y - 0.08f;
+            transform.localScale = scalee;
+        }
+        
+        GM.GetComponent<TitleBtnEvt>().closeWindow();
+    }
+
+    
+
+
+    public void ZoomInSpecial()
+    {
+                
+        //합성창
+        if (GM.GetComponent<TitleCardEvt>().setCardMix_i == 1)
+        {
+            if (GM.GetComponent<TitleCardEvt>().popInt == 1)
+            {
+                StartCoroutine("popupZoomInSpecial");
+                GM.GetComponent<TitleCardEvt>().popInt = 0;
+            }
+
+        }
+        else
+        {
+            StartCoroutine("popupZoomInSpecial");
+        }
+
+        
+
+    }
+
+    IEnumerator popupZoomInSpecial()
+    {
+        /*
+        try
+        {
+            transform.localScale = scalee;
+        }
+        catch (Exception ex)
+        {
+            Debug.Log("inactive");
+        }
+        */
+        scalee.x = 0.9f;
+        scalee.y = 0.9f;
+        transform.localScale = scalee;
+        scalee = transform.localScale;
+        while (scalee.x < 1.1f)
+        {
+            yield return new WaitForSeconds(0.01f);
+            scalee.x = scalee.x + 0.03f;
+            scalee.y = scalee.y + 0.03f;
+            transform.localScale = scalee;
+        }
+
+        while (scalee.x >= 1.05f)
+        {
+            yield return new WaitForSeconds(0.01f);
+            scalee.x = scalee.x - 0.03f;
+            scalee.y = scalee.y - 0.03f;
+            transform.localScale = scalee;
+        }
+
+    }
+
+
+}
